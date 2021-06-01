0a4c722390c27c6224c6d158101f3b56d9b00de3
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-06-01 13:00:27 +0600

    traversing dom

--- JS_DOM/script.js
+++ JS_DOM/script.js
@@ -1,3 +1,54 @@
-for(let element of document.all) {
-    console.log(element);
-}
+// GET ELEMENT BY ID
+
+
+// let headerElement = document.getElementById('header');
+
+//console.log(headerElement.textContent);
+//console.log(headerElement.innerText);
+//console.log(headerElement.innerHTML);
+
+//headerElement.style.fontSize = '50px';
+
+
+// GET ELEMENT BY Class
+
+
+//console.log(document.getElementsByClassName('item'));
+
+// let itemUL = document.getElementById('items');
+// let items = itemUL.getElementsByClassName('item');
+// for(let i=0; i<items.length; i++) {
+//     items[i].style.color = 'red';
+// }
+
+
+// GET ELEMENT BY tagname
+
+
+// let itemed = document.getElementsByTagName('h2');
+
+// console.log(itemed)
+
+
+// Query Selector
+
+
+// let header = document.querySelector('.header');
+
+// console.log(header);
+
+// let newTask = document.querySelector('#new-task');
+
+// console.log(newTask);
+
+let lastItem = document.querySelector('.item:nth-child(2)');
+
+//console.log(lastItems);
+
+//lastItems.style.color = 'orange';
+
+// for(let element of lastItem){
+//     element.style.color = 'orange';
+// }
+
+lastItem.style.color = 'blue';
