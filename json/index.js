25d08ebe748a4d754fb01908e42782bd924938aa
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-07-07 04:50:13 +0600

    path & schema

---
+++ json/index.js
@@ -0,0 +1,46 @@
+// const data = require('./john_green.json');
+
+// Access
+
+//console.log(data);
+
+// console.log(data.duckling[0]);
+
+// console.log(data.duckling[0].name);
+
+
+// Modify
+
+// data.duckling[1].name = "Dolphin";
+
+// console.log(data.duckling[1].name);
+
+
+// Delete
+
+// delete data.duckling[1].name;
+
+// console.log(data);
+
+
+// const data = require('./dan_brown.json');
+
+// console.log(data.okapia[1].duckling[1].house);
+
+
+// const data = require('./looping.json');
+
+
+// for(x in data){
+//     console.log(x);
+//     console.log(data[x]);
+// }
+
+// const datum = {
+//     name: "fair",
+//     genre: "heavy_metal"
+// }
+
+// console.log(JSON.stringify(datum));
+
+console.log(JSON.parse('{"name" : "faizul"}'));
